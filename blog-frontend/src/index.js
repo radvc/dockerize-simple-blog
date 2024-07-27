224a26b21477f1bf683dfdccf02bdb35c2fbeb83
radvc <cadagryanangelo@gmail.com>
2024-07-27 14:44:38 +0800

    add react router

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -3,13 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import ViewArticle from './pages/ViewArticle';
+import EditArticle from './pages/EditArticle';
+import NewArticle from './pages/NewArticle';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/show/:id',
+    element: <ViewArticle />
+  },
+  {
+    path: '/edit/:id',
+    element: <EditArticle />
+  },
+  {
+    path: '/new-article',
+    element: <NewArticle />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
